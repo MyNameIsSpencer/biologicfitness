03a8bbab519963f8d019877a0138adcd2ffab62c
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-09-07 00:11:11 -0400

    add updatePlan function

--- Home.js
+++ Home.js
@@ -1,17 +1,22 @@
 let goals = document.getElementById("goals").options;
-
-// var select = document.getElementById('color');
-// var currentOpt = select.options[select.selectedIndex];
+let plans = document.getElementById("plans").options;
 
 
 
-function updateContactForm(choice) {
-  let currentGoal = goals.selectedIndex;
+function updateGoal(choice) {
   for (let i=0; i < goals.length; i ++) {
     if (goals[i].value === choice) {
       goals.selectedIndex = i;
     }
   }
   console.log(goals.selectedIndex);
+}
 
+function updatePlan(choice) {
+  for (let i=0; i < plans.length; i ++) {
+    if (plans[i].value === choice) {
+      plans.selectedIndex = i;
+    }
+  }
+  console.log(plans.selectedIndex);
 }
